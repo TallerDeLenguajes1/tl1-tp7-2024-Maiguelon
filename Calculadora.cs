ea8ccaad57f30a59c13f8a8ddf06f10e4d773985
Maiguelon <miguel_carretero@outlook.com>
2024-08-05 19:58:47 -0300

    comenzando

---
+++ Calculadora.cs
@@ -0,0 +1,12 @@
+namespace EspacioCalculadora;
+
+class Calculadora {
+
+    // Campo
+    private double resultado;
+
+    // Metodos
+    public void sumar (double termino) {
+        resultado = resultado + termino;
+    }
+}
