23053b283e4095303b1404f2906cc96924eb8942
Maiguelon <miguel_carretero@outlook.com>
2024-08-16 00:31:15 -0300

    metodos

--- Empleado.cs
+++ Empleado.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EspacioEmpleado;
 
 // Clase empleado con sus chiches
@@ -9,6 +11,23 @@ class Employee {
     private DateTime FechaIngreso;
     private double SueldoBasico;
     private Cargo CargoEmpleado;
+    // Metpdp para calcular antiguedad
+    public double Antiguedad() {
+        TimeSpan antiguedad = DateTime.Now - FechaIngreso;
+        double antiguedadNumero = antiguedad.Days / 365.0;
+        return antiguedadNumero;
+    }
+    // Metodo para la edad
+    public int Edad() {
+        TimeSpan edad = DateTime.Now - FechaNacimiento;
+        int edadNumero = edad.Days / 365; // no tomo en cuenta años bisiestos...
+        return edadNumero;
+    }
+    // Metodo para jubilacion
+    public int Jubilacion() {
+        int jubilacion = 65 - Edad(); 
+        return jubilacion >= 0 ? jubilacion : 0; // En caso de que tenga más de 65
+    }
 }
 
 public enum Cargo {
